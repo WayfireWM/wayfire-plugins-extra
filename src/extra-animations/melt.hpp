fea0b378a76cd0aee35c7b8150b5e4b96752e030
Scott Moreau <oreaus@gmail.com>
2025-08-07 11:52:35 -0600

    extra-animations: Add melt animation

---
+++ src/extra-animations/melt.hpp
@@ -0,0 +1,326 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2025 Scott Moreau <oreaus@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <wayfire/opengl.hpp>
+#include <wayfire/core.hpp>
+#include <wayfire/view-transform.hpp>
+
+
+static const char *melt_vert_source =
+    R"(
+#version 100
+
+attribute highp vec2 position;
+attribute highp vec2 uv_in;
+
+varying highp vec2 uvpos;
+
+void main() {
+
+    gl_Position = vec4(position.xy, 0.0, 1.0);
+    uvpos = uv_in;
+}
+)";
+
+static const char *melt_frag_source =
+    R"(
+#version 100
+@builtin_ext@
+@builtin@
+
+precision highp float;
+
+varying highp vec2 uvpos;
+uniform float time;
+uniform float distortion;
+
+float rand(vec2 n) {
+    return fract(sin(dot(n, vec2(12.9898, 4.1414))) * 43758.5453);
+}
+
+// Noise function from: https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83
+float noise(vec2 n) {
+    const vec2 d = vec2(0.0, 1.0);
+    vec2 b = floor(n), f = smoothstep(vec2(0.0), vec2(1.0), fract(n));
+    return mix(mix(rand(b), rand(b + d.yx), f.x), mix(rand(b + d.xy), rand(b + d.yy), f.x), f.y);
+}
+
+void main() {
+    vec2 uv = uvpos;
+
+    vec2 distorted_uv =
+        uv + (vec2(
+        noise(uv * distortion + time / distortion),
+        noise(uv * distortion - time / distortion)) *
+        0.1 * (1.0 - time) - (1.0 - time) * 0.05);
+
+    if (distorted_uv.x < 0.0 || distorted_uv.y < 0.0 || distorted_uv.x > 1.0 || distorted_uv.y > 1.0) {
+        discard;
+    }
+
+    vec4 color = get_pixel(distorted_uv);
+
+    gl_FragColor = color;
+}
+)";
+
+namespace wf
+{
+namespace melt
+{
+using namespace wf::scene;
+using namespace wf::animate;
+using namespace wf::animation;
+
+static std::string melt_transformer_name = "animation-melt";
+
+wf::option_wrapper_t<wf::animation_description_t> melt_duration{"extra-animations/melt_duration"};
+wf::option_wrapper_t<int> melt_distortion_factor{"extra-animations/melt_distortion_factor"};
+
+class melt_animation_t : public duration_t
+{
+  public:
+    using duration_t::duration_t;
+};
+class melt_transformer : public wf::scene::view_2d_transformer_t
+{
+  public:
+    wayfire_view view;
+    wf::output_t *output;
+    OpenGL::program_t program;
+    wf::auxilliary_buffer_t buffer;
+    melt_animation_t progression{melt_duration};
+
+    class simple_node_render_instance_t : public wf::scene::transformer_render_instance_t<transformer_base_node_t>
+    {
+        wf::signal::connection_t<node_damage_signal> on_node_damaged =
+            [=] (node_damage_signal *ev)
+        {
+            push_to_parent(ev->region);
+        };
+
+        melt_transformer *self;
+        wayfire_view view;
+        damage_callback push_to_parent;
+
+      public:
+        simple_node_render_instance_t(melt_transformer *self, damage_callback push_damage,
+            wayfire_view view) : wf::scene::transformer_render_instance_t<transformer_base_node_t>(self,
+                push_damage,
+                view->get_output())
+        {
+            this->self = self;
+            this->view = view;
+            this->push_to_parent = push_damage;
+            self->connect(&on_node_damaged);
+        }
+
+        ~simple_node_render_instance_t()
+        {}
+
+        void schedule_instructions(
+            std::vector<render_instruction_t>& instructions,
+            const wf::render_target_t& target, wf::region_t& damage) override
+        {
+            instructions.push_back(render_instruction_t{
+                        .instance = this,
+                        .target   = target,
+                        .damage   = damage,
+                    });
+        }
+
+        void transform_damage_region(wf::region_t& damage) override
+        {
+            damage |= self->output->get_relative_geometry();
+        }
+
+        void render(const wf::scene::render_instruction_t& data) override
+        {
+            auto bb  = self->get_bounding_box();
+            auto og  = self->output->get_relative_geometry();
+            auto tex = wf::gles_texture_t{get_texture(1.0)};
+
+            const float vertices[] = {
+                -1.0, -1.0,
+                1.0f, -1.0,
+                1.0f, 1.0f,
+                -1.0, 1.0f
+            };
+            wf::pointf_t offset1{-float(bb.x) / bb.width, -float(og.height - (bb.y + bb.height)) / bb.height};
+            wf::pointf_t offset2{float(og.width) / bb.width + offset1.x,
+                float(og.height) / bb.height + offset1.y};
+            const float uv[] = {
+                float(offset1.x), float(offset2.y),
+                float(offset2.x), float(offset2.y),
+                float(offset2.x), float(offset1.y),
+                float(offset1.x), float(offset1.y),
+            };
+            auto progress = self->progression.progress();
+
+            data.pass->custom_gles_subpass([&]
+            {
+                self->buffer.allocate({og.width, og.height});
+                wf::gles::bind_render_buffer(self->buffer.get_renderbuffer());
+                wf::gles_texture_t final_tex{self->buffer.get_texture()};
+                OpenGL::clear(wf::color_t{0.0, 0.0, 0.0, 0.0}, GL_COLOR_BUFFER_BIT);
+                self->program.use(wf::TEXTURE_TYPE_RGBA);
+                self->program.attrib_pointer("position", 2, 0, vertices);
+                self->program.attrib_pointer("uv_in", 2, 0, uv);
+                self->program.uniform1f("time", progress);
+                self->program.uniform1f("distortion", float(melt_distortion_factor));
+                self->program.set_active_texture(tex);
+                GL_CALL(glDrawArrays(GL_TRIANGLE_FAN, 0, 4));
+
+                wf::gles::bind_render_buffer(data.target);
+                for (auto box : data.damage)
+                {
+                    wf::gles::render_target_logic_scissor(data.target, wlr_box_from_pixman_box(box));
+                    OpenGL::render_transformed_texture(final_tex, og,
+                        wf::gles::render_target_orthographic_projection(data.target),
+                        glm::vec4(1.0, 1.0, 1.0, std::clamp(progress * 3.0, 0.0, 1.0)), 0);
+                }
+
+                GL_CALL(glBindTexture(GL_TEXTURE_2D, 0));
+                self->program.deactivate();
+                self->buffer.free();
+            });
+        }
+    };
+
+    melt_transformer(wayfire_view view, wf::geometry_t bbox) : wf::scene::view_2d_transformer_t(view)
+    {
+        this->view = view;
+        if (view->get_output())
+        {
+            output = view->get_output();
+            output->render->add_effect(&pre_hook, wf::OUTPUT_EFFECT_PRE);
+        }
+
+        wf::gles::run_in_context([&]
+        {
+            program.compile(melt_vert_source, melt_frag_source);
+        });
+    }
+
+    wf::effect_hook_t pre_hook = [=] ()
+    {
+        output->render->damage_whole();
+    };
+
+    void gen_render_instances(std::vector<render_instance_uptr>& instances,
+        damage_callback push_damage, wf::output_t *shown_on) override
+    {
+        instances.push_back(std::make_unique<simple_node_render_instance_t>(
+            this, push_damage, view));
+    }
+
+    void init_animation(bool melt)
+    {
+        if (melt)
+        {
+            this->progression.reverse();
+        }
+
+        this->progression.start();
+    }
+
+    virtual ~melt_transformer()
+    {
+        if (output)
+        {
+            output->render->rem_effect(&pre_hook);
+        }
+
+        wf::gles::run_in_context_if_gles([&]
+        {
+            program.free_resources();
+        });
+    }
+};
+
+class melt_animation : public animation_base_t
+{
+    wayfire_view view;
+
+  public:
+    void init(wayfire_view view, wf::animation_description_t dur, animation_type type) override
+    {
+        this->view = view;
+        pop_transformer(view);
+        auto bbox = view->get_transformed_node()->get_bounding_box();
+        auto tmgr = view->get_transformed_node();
+        auto node = std::make_shared<melt_transformer>(view, bbox);
+        tmgr->add_transformer(node, wf::TRANSFORMER_HIGHLEVEL + 1, melt_transformer_name);
+        node->init_animation(type & WF_ANIMATE_HIDING_ANIMATION);
+    }
+
+    void pop_transformer(wayfire_view view)
+    {
+        if (view->get_transformed_node()->get_transformer(melt_transformer_name))
+        {
+            view->get_transformed_node()->rem_transformer(melt_transformer_name);
+        }
+    }
+
+    bool step() override
+    {
+        if (!view)
+        {
+            return false;
+        }
+
+        auto tmgr = view->get_transformed_node();
+        if (!tmgr)
+        {
+            return false;
+        }
+
+        if (auto tr =
+                tmgr->get_transformer<wf::melt::melt_transformer>(melt_transformer_name))
+        {
+            auto running = tr->progression.running();
+            if (!running)
+            {
+                pop_transformer(view);
+                return false;
+            }
+
+            return running;
+        }
+
+        return false;
+    }
+
+    void reverse() override
+    {
+        if (auto tr =
+                view->get_transformed_node()->get_transformer<wf::melt::melt_transformer>(
+                    melt_transformer_name))
+        {
+            tr->progression.reverse();
+        }
+    }
+};
+}
+}
